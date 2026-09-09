f5b002964b098e1124af2fd46966e3d7b0d69c15
Matheus David <matdavidbs@gmail.com>
2026-09-09 00:40:26 -0300

    Corrige CORS para producao

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -38,7 +38,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://weblue-production.up.railway.app"
     ],
     allow_credentials=False,
     allow_methods=["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
